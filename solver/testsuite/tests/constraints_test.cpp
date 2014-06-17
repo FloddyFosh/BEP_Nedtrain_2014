c0184d7d1d20d1f762fc174c0439648a3fed644d
martijnn2008 <martijnn2008@gmail.com>
2014-06-17 12:20:41 +0200

    ConstraintsTests gefixed

--- solver/testsuite/tests/constraints_test.cpp
+++ solver/testsuite/tests/constraints_test.cpp
@@ -126,9 +126,22 @@ TEST_F(ConstraintsTest, UpperLimit_2) {
 }
 
 TEST_F(ConstraintsTest, Locked_1) {
-    ADD_FAILURE();
+    cstr.setLocked("A", true);
+    EXPECT_EQ(true, cstr.getLocked(0));
 }
 
 TEST_F(ConstraintsTest, Locked_2) {
-    ADD_FAILURE();
+    cstr.setLocked("B", false);
+    EXPECT_EQ(false, cstr.getLocked(0));
+}
+
+TEST_F(ConstraintsTest, Locked_3) {
+    cstr.setLocked("A", true);
+    cstr.setLocked("B", false);
+    EXPECT_THROW(cstr.getLocked(2), NoSuchVariableException);
+}
+
+TEST_F(ConstraintsTest, Locked_4) {
+    EXPECT_THROW(cstr.getLocked(0), NoSuchVariableException);
+    EXPECT_THROW(cstr.getLocked(-1), NoSuchVariableException);
 }
